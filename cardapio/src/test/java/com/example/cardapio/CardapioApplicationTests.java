14e84be8ecd2afee990de286b113b9f362a413cb
Jorginho1997z <jorgesad200@gmail.com>
2024-04-28 20:15:15 -0300

    backend-cardapi0-digital

---
+++ cardapio/src/test/java/com/example/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioApplication.class,args);
+	}
+}
